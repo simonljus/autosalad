6f2d10545eb4fa494ed122e3d07d6fbea07764a8
simonljus <simon.ljus94@gmail.com>
2020-03-02 21:54:04 +0100

    Excel file support,added instructions in readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # autosalad
-Autofill form at Citysallad 
+Autofill form at [Citysallad](https://citysallad.se)
+
+Uses [sheetJS](https://github.com/SheetJS/sheetjs) to read data from Excel file and [Puppeteer](https://github.com/puppeteer/puppeteer/) to fill the form in a Chrome browser window.
+## Instructions
+In command prompt:
+run `npm install` and `node salad`
+
+Make sure that:
+* The Chrome browser is closed from the start.
+* The Chrome browser path is configured correctly
+* The column names in the Excel file are specified.
+* The path to the Excel file is correct.
+
+
+
+
--- salad.js
+++ salad.js
@@ -1,11 +1,39 @@
 const puppeteer = require("puppeteer-core");
 const fs = require("fs");
-const papa = require("papaparse");
+const XLSX = require('XLSX');
 //############################## MODIFICATION REQUIRED ##############################################
 const URL="https://citysallad.se/uppsala/catering/"
 const CHROMEPATH="/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe" //set the path where you have chrome installed
-const FILEPATH="salads.csv" // path to CSV FILE
+const FILEPATH="salad.xlsx" // path to excel file
 
+//Enter the column names in the excel file
+const sum_columns={
+    "ramlosa-naturel":"RN",
+    "ramlosa-citrus":"RC",
+    "ramlosa-granatapple":"RG",
+    "coca-cola":"C",
+    "coca-cola-zero":"CZ",
+    "zingo":"Z",
+    "beer":"Ö",
+    "01":"NR1",
+    "02":"2",
+    "03":"3",
+    "04":"4",
+    "05":"5",
+    "06":"6",
+    "07":"7",
+    "08":"8",
+    "09":"9",
+    "10":"10",
+    "11":"11",
+    "12":"12",
+}
+// Enter the comment column
+const concat_columns={
+    "other":"Övrigt"
+}
+// If you have a summation row or similar, enter the specification here and it will be ignored
+const ignore_row={column:"Namn",value:"Summa"}
 
 //set deliveryfinformation
 const deliveryInformation ={
@@ -22,64 +50,94 @@ const deliveryInformation ={
     date:"2020-03-14",//want this last because input is wierd because of datepicker
 };
 
-//set how many of each salad
-const salads = {
-    "01":1,
+//set how many of each salad if no excel file
+let salads = {
+    "01":0,
     "02":0,
-    "03":1,
+    "03":0,
     "04":0,
-    "05":1,
+    "05":0,
     "06":0,
-    "07":1,
+    "07":0,
     "08":0,
-    "09":1,
+    "09":0,
     "10":0,
-    "11":1,
+    "11":0,
     "12":0
 }
 
-//set how many of each beverage
-const beverages ={
-    "ramlosa-naturel":1,
+//set how many of each beverage if no excel file
+let beverages ={
+    "ramlosa-naturel":0,
     "ramlosa-citrus":0,
-    "ramlosa-granatapple":1,
+    "ramlosa-granatapple":0,
     "coca-cola":0,
-    "coca-cola-zero":1,
-    "zingo":1,
+    "coca-cola-zero":0,
+    "zingo":0,
     "beer":0
 }
 
-//set other information
-const other={
-    "other":"Övrig information"
+//set other information if no excel file
+let other={
+    "other":""
 }
+
 
 //############################## NO MORE MODIFICATION REQUIRED ##############################################
 
-autofill()
+if(FILEPATH){
+    autofillSheet()
+}
+else{
+    fillForm()
+}
 
-function autofill(){
-    fs.stat(FILEPATH, function(err, stat) {
-        if(err == null) {
-            const file = fs.createReadStream(FILEPATH);
-            papa.parse(file, {
-                worker: true, // Don"t bog down the main thread if its a big file
-                step: function(result) {
-                   console.log("TODO: PREFILL SALAD HERE")
-                },
-                complete: function(results, file) {
-                    fillForm()
-                }
-            });
-        } else if(err.code === 'ENOENT') {
-            // file does not exist
-            console.warn("file does not exist")
-            fillForm()
-        } else {
-            console.warn('Some other error with file: ', err.code);
-        }
-    });
-   
+
+async function autofillSheet(){
+    const wb = XLSX.readFile(FILEPATH)
+    const totals ={}
+    const orders =XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]).filter(order=>order[ignore_row.column] !==ignore_row.value)
+    //console.log(orders) // orders that will be read
+    Object.entries(sum_columns).forEach(([key,columnName])=>{
+        const reducer = (accumulator, currentValue) => accumulator + numericValueOfCell(currentValue[columnName]);
+        totals[key] = orders.reduce(reducer,0)
+    })
+    Object.entries(concat_columns).forEach(([key,columnName])=>{
+        const reducer = (accumulator, currentValue) => {
+            let val =stringValueOfCell(currentValue[columnName])
+            val  = val ? val  + "\n" : "";
+            return accumulator + val
+        };
+        console.log(key)
+        totals[key]=orders.reduce(reducer,"")
+    })
+    copyExisting(totals,beverages)
+    copyExisting(totals,salads)
+    copyExisting(totals,other)
+    console.log(totals)
+    await fillForm()
+}
+function stringValueOfCell(val){
+    if(typeof(val)=== "string"){
+        return val.trim()
+    }
+    else{
+        return null
+    }
+}
+function copyExisting(fromObject,toObject){
+    Object.entries(fromObject).forEach(([key,val]) => key in toObject? toObject[key] = val : null)
+}
+function numericValueOfCell(val){
+    if(typeof(val) === "number"){
+        return val > 0 ? val : 0
+    }
+    else if(typeof(val) === "string"){
+        return val.trim() ? 1: 0
+    }
+    else{
+      return val ? 1 : 0
+    }
 }
 
 async function fillForm(){
